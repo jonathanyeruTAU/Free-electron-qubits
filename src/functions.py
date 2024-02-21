0f338a955471acb3255ce198d3082caf85320b65
jonathanyeru <jonathanyerushalmi36@gmail.com>
2024-02-21 21:35:45 +0200

    probably make some changes

--- src/functions.py
+++ src/functions.py
@@ -2,10 +2,13 @@ import numpy as np
 import pint
 import scipy.constants as constants
 from scipy import integrate
+import matplotlib.pyplot as plt
 
 
 def gaussian(mu, sigma):
     return lambda x: np.exp((-((x - mu) ** 2)) / (2 * sigma ** 2)) / (np.sqrt(2 * np.pi * sigma ** 2))
+
+
 
 
 ureg = pint.UnitRegistry(system='SI')
@@ -24,7 +27,7 @@ t_final = 280 * pow(10, -15)
 t_initial = 0
 u = gaussian(mu=t_final / 2, sigma=t_final / 3)
 u_squared = lambda t: u(t) ** 2
-integral_of_u_squared = integrate.quad(u_squared, t_initial, t_final)  # TODO: give this units
+integral_of_u_squared = integrate.quad(u_squared, t_initial, t_final)
 
 
 def get_electron_wave_function(electron_speed, g_squared, electric_field_strength):
@@ -57,7 +60,53 @@ def get_electron_wave_function(electron_speed, g_squared, electric_field_strengt
 
 
 def get_electron_probability_function(wave_function):
-    return lambda x, y: np.abs(wave_function(x, y))
+    return lambda x, y: np.abs(wave_function(x, y)) ** 2
+
 
 def simulate_shape(electron_probability_function):
-    pass
+    x = np.linspace(0, 1, 1000)
+    y = np.linspace(0, 1, 1000)
+    X, Y = np.meshgrid(x, y)
+    Z = electron_probability_function(X, Y)
+
+    # Plot
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_surface(X, Y, Z, cmap='viridis')
+
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_zlabel('Z Label')
+
+    plt.show()
+
+
+def make_2d_gaussian(mu_x, mu_y, sigma_x, sigma_y):
+    """
+    Create a 2D Gaussian function with specified parameters.
+
+    Parameters:
+        mu_x (float): x-coordinate of the center of the Gaussian.
+        mu_y (float): y-coordinate of the center of the Gaussian.
+        sigma_x (float): Standard deviation along the x-axis.
+        sigma_y (float): Standard deviation along the y-axis.
+
+    Returns:
+        function: A function representing the 2D Gaussian with the specified parameters.
+    """
+    def gaussian_2d(x, y):
+        """
+        Compute the value of the 2D Gaussian function at given coordinates (x, y).
+
+        Parameters:
+            x (float or numpy array): x-coordinate(s) of the point(s) to evaluate.
+            y (float or numpy array): y-coordinate(s) of the point(s) to evaluate.
+
+        Returns:
+            float or numpy array: The value(s) of the 2D Gaussian function at the given coordinates.
+        """
+        exponent = -((x - mu_x)**2 / (2 * sigma_x**2) + (y - mu_y)**2 / (2 * sigma_y**2))
+        normalization = 1 / (2 * np.pi * sigma_x * sigma_y)
+        return normalization * np.exp(exponent)
+
+    return gaussian_2d
