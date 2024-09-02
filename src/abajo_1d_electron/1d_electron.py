ddf74f53844f61cd014f90ab9ffa3246c9af81fa
jonathanyeru <jonathanyerushalmi36@gmail.com>
2024-09-02 13:58:44 +0300

    changed the plot, made it a single plot now

--- src/abajo_1d_electron/1d_electron.py
+++ src/abajo_1d_electron/1d_electron.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 from multiprocessing import Pool
+from matplotlib.ticker import ScalarFormatter
 
 
 
@@ -15,7 +16,7 @@ def main():
     NA = 0.01
     lambda_e_perpendicular = lambda_0 / NA
 
-    x_f = np.linspace(-3 * lambda_e_perpendicular, 3 * lambda_e_perpendicular, 500)
+    x_f = np.linspace(-2 * lambda_e_perpendicular, 2 * lambda_e_perpendicular, 500)
 
     # focus_x = get_function_focus(target_phase=lambda x: 2 * x / R_max, R_max=R_max)
     # plt.plot(x_f, focus_x(x_f), label="2x / R_max")
@@ -29,17 +30,84 @@ def main():
     # focus_sin = get_function_focus(target_phase=sin_func, R_max=R_max)
     # plt.plot(x_f, focus_sin(x_f), label="2x/Rmax * sin(20*pi*x/Rmax)")
 
+    # move_by = 0.0000025
+    # asymmetric_sin_func = lambda x: 2 * (x + move_by) / R_max * np.sin(4 * np.pi * (x + move_by) / R_max) + 0.2
+    # focus_asymmetric_sin = get_function_focus(target_phase=asymmetric_sin_func, R_max=R_max)
+    # plt.plot(x_f, focus_asymmetric_sin(x_f), label="asymmetric sin")
 
-    move_by = 0.0000025
-    asymmetric_sin_func = lambda x: 2 * (x + move_by) / R_max * np.sin(4 * np.pi * (x + move_by) / R_max) + 0.2
-    focus_asymmetric_sin = get_function_focus(target_phase=asymmetric_sin_func, R_max=R_max)
-    plt.plot(x_f, focus_asymmetric_sin(x_f), label="asymmetric sin")
-
-    # x_sin = np.linspace(-R_max, R_max, 1000)
+    x_input = np.linspace(-R_max, R_max, 1000)
     # plt.plot(x_sin, sin_func(x_sin))
 
-    plt.grid()
-    plt.legend()
+    move_by = 0.0000025
+    plots_data = [
+        {
+            "input_func": lambda x: 2 * x / R_max,
+            "title": r"$\frac{2x}{R_{max}}$"
+        },
+        {
+            "input_func": lambda x: -2 * x / R_max,
+            "title": r"$-\frac{2x}{R_{max}}$"
+        },
+        {
+            "input_func": lambda x: 2 * x / R_max * np.sin(4 * np.pi * x / R_max),
+            "title": r"$\frac{2x}{R_{max}} \cdot \sin\left(\frac{20\pi x}{R_{max}}\right)$"
+        },
+        {
+            "input_func": lambda x: 2 * (x + move_by) / R_max * np.sin(4 * np.pi * (x + move_by) / R_max) + 0.2,
+            "title": "asymmetric sin"
+        }
+    ]
+    colors = ['blue', 'green', 'red', plt.rcParams['axes.prop_cycle'].by_key()['color'][0]]
+
+    tags = ["(a)", "(b)", "(c)", "(d)", "(e)", "(f)", "(g)", "(h)"]
+
+    fig, axs = plt.subplots(4, 2, figsize=(8, 10))
+
+    for i, plot_data in enumerate(plots_data):
+        input_func = plot_data["input_func"]
+        title = plot_data["title"]
+
+        ax = axs[i, 0]
+        ax.plot(x_input, input_func(x_input), color=colors[i])
+        ax.set_title(title)
+        ax.grid(True)
+        ax.text(0, 1, tags[i*2], fontsize=12, color='black',
+                ha='left', va='top', transform=ax.transAxes)
+
+        ax.axhline(0, color='black', linewidth=1)
+        ax.axvline(0, color='black', linewidth=1)
+
+        if i < len(plots_data) - 1:
+            ax.set_xticklabels([])
+        else:
+            ax.xaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+            ax.xaxis.get_major_formatter().set_powerlimits((-5, 5))
+
+
+        focus_func = get_function_focus(target_phase=input_func, R_max=R_max)
+        ax = axs[i, 1]
+        ax.plot(x_f, focus_func(x_f), color=colors[i])
+        ax.grid(True)
+        ax.text(0, 1, tags[i * 2 + 1], fontsize=12, color='black',
+                ha='left', va='top', transform=ax.transAxes)
+        ax.axhline(0, color='black', linewidth=1)
+        ax.axvline(0, color='black', linewidth=1)
+
+        if i < len(plots_data) - 1:
+            ax.set_xticklabels([])
+        else:
+            ax.xaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+            ax.xaxis.get_major_formatter().set_powerlimits((-4, 4))
+
+
+
+    fig.text(0.25, 0.96, r"Imprinted phase $\frac{\varphi(x)}{\pi} (rad)$", ha='center', va='center', fontsize=14)
+    fig.text(0.75, 0.96, r"Wave func at focus $\mathrm{Re}\{\psi(x_f)\}$ (arb. units)", ha='center', va='center', fontsize=14)
+
+    # Adjust layout to avoid overlap
+    plt.tight_layout(rect=[0, 0, 1, 0.95])
+
+    # Show the plot
     plt.show()
 
 def double_gaussian_matching():
