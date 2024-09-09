35fe5241973fa673ebc5b41fd957cf7ac9c12730
jonathanyeru <jonathanyerushalmi36@gmail.com>
2024-09-09 23:13:24 +0300

    added plots

---
+++ src/plots_makers_for_paper/gaussian_makers.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def gaussian(mu, sigma):
+    return lambda x: np.exp((-((x - mu) ** 2)) / (2 * sigma ** 2)) / (np.sqrt(2 * np.pi * sigma ** 2))
+
+
+def make_01_plots():
+    left_gaussian = gaussian(mu=-1, sigma=0.3)
+    right_gaussian = gaussian(mu=1, sigma=0.3)
+
+    hadamard = lambda x: 1 / np.sqrt(2) * (gaussian(mu=-1, sigma=0.3)(x) + gaussian(mu=1, sigma=0.3)(x))
+
+    x = np.linspace(-2, 2, 1000)
+
+    plt.plot(x, left_gaussian(x), label=r"$|0\rangle$")
+    plt.plot(x, right_gaussian(x), label=r"$|1\rangle$")
+    plt.plot(x, hadamard(x), label=r"$\frac{1}{\sqrt{2}}(|0\rangle + |1\rangle)$")
+    plt.grid()
+    plt.legend(loc='best')  # Position legend in the upper left
+    plt.axhline(0, color='black', linewidth=1)  # Horizontal axis line
+    plt.axvline(0, color='black', linewidth=1)  # Vertical axis line
+    plt.ylim(0, 1.75)
+    plt.ylabel(r"$|\psi(x)|^2$")
+    plt.xlabel(r"x")
+    plt.show()
+
+
+if __name__ == '__main__':
+    make_01_plots()
