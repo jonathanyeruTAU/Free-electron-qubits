76e88e148067c0ecb3d66a7bf39cf82663867c7e
jonathanyeru <jonathanyerushalmi36@gmail.com>
2024-10-27 12:23:18 +0200

    added the the yticks

--- src/plots_makers_for_paper/gaussian_makers.py
+++ src/plots_makers_for_paper/gaussian_makers.py
@@ -7,12 +7,16 @@ def gaussian(mu, sigma):
 
 
 def make_01_plots():
-    left_gaussian = gaussian(mu=-1, sigma=0.3)
-    right_gaussian = gaussian(mu=1, sigma=0.3)
-
+    sigma = 0.3
+    left_gaussian = gaussian(mu=-1, sigma=sigma)
+    right_gaussian = gaussian(mu=1, sigma=sigma)
+    max_value_of_gaussian = left_gaussian(-1)
     hadamard = lambda x: 1 / np.sqrt(2) * (gaussian(mu=-1, sigma=0.3)(x) + gaussian(mu=1, sigma=0.3)(x))
 
+
     x = np.linspace(-2, 2, 1000)
+
+    y_ticks = [max_value_of_gaussian, max_value_of_gaussian / np.sqrt(2)]
 
     plt.plot(x, left_gaussian(x), label=r"$|0\rangle$")
     plt.plot(x, right_gaussian(x), label=r"$|1\rangle$")
@@ -21,10 +25,14 @@ def make_01_plots():
     plt.axhline(0, color='black', linewidth=1)  # Horizontal axis line
     plt.axvline(0, color='black', linewidth=1)  # Vertical axis line
     plt.ylim(0, 1.75)
-    plt.ylabel(r"$|\psi(x)|^2/\sigma$", fontsize=14)
+    plt.ylabel(r"$|\psi(x)|^2\cdot(\sigma\sqrt{2\pi})$", fontsize=14)
     plt.xlabel(r"$x$", fontsize=14)
     plt.xticks([-1, 0, 1], [r'$-\mu$', '0', r'$\mu$'])
-    plt.yticks([])
+    plt.yticks(y_ticks, ['1', r'$\frac{1}{\sqrt{2}}$'])
+    for i in y_ticks:
+        plt.axhline(y=i, color='gray', linestyle='--', linewidth=0.5)
+
+
     plt.show()
 
 
